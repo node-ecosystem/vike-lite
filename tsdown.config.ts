38086d8a290f7f4c5f2f5261cf6aa192f6cf2ff9
rtritto <ariele.1994@gmail.com>
2026-06-24 16:04:00 +0200

    fix: export types

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,7 @@ import type { UserConfig } from 'tsdown'
 
 export default {
   entry: {
+    'index': 'src/index.ts',
     'vite': 'src/vite-plugin.ts',
     'server': 'src/server/index.ts',
     'shared': 'src/shared/index.ts'
