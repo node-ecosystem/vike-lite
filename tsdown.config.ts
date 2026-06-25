81e60411ac7c2a68ef8b97411c8e16826193806a
rtritto <ariele.1994@gmail.com>
2026-06-25 22:31:06 +0200

    export matchRoute as internal

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,7 +5,7 @@ export default {
     'index': 'src/index.ts',
     'vite': 'src/vite-plugin.ts',
     'server': 'src/server/index.ts',
-    'shared': 'src/shared/index.ts',
+    '__internal/shared/matchRoute': 'src/shared/matchRoute.ts',
     '__internal/server/store': 'src/server/store.ts'
   }
 } satisfies UserConfig
