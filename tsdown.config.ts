db970b5f8e1bd3bbffe388c1a2f7b2c9372c0edc
rtritto <ariele.1994@gmail.com>
2026-06-25 22:25:56 +0200

    export store as internal

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,6 +5,7 @@ export default {
     'index': 'src/index.ts',
     'vite': 'src/vite-plugin.ts',
     'server': 'src/server/index.ts',
-    'shared': 'src/shared/index.ts'
+    'shared': 'src/shared/index.ts',
+    '__internal/server/store': 'src/server/store.ts'
   }
 } satisfies UserConfig
