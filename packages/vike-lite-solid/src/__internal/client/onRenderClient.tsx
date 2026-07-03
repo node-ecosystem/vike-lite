fc08ed6524f5faca0af6a0c4dc7aa9dab5f27f43
rtritto <ariele.1994@gmail.com>
2026-07-03 16:10:59 +0200

    fix(vike-lite-solid): on 404 handle hydration

--- packages/vike-lite-solid/src/__internal/client/onRenderClient.tsx
+++ packages/vike-lite-solid/src/__internal/client/onRenderClient.tsx
@@ -20,17 +20,47 @@ export default async function onRenderClient(clientOptions: Omit<RouterProps, 'i
   initialContext.urlPathname = pathname
   initialContext.urlOriginal = urlOriginal
 
-  const matched = matchRoute(pathname, clientOptions.routes)
-  if (matched) {
-    const [PageMod, LayoutMod, HeadMod] = await Promise.all([
-      matched.route.Page(),
-      matched.route.Layout?.() ?? null,
-      matched.route.Head?.() ?? null
-    ])
-    initialView = {
-      Page: PageMod.Page ?? PageMod.default,
-      Layout: LayoutMod?.Layout ?? LayoutMod?.default ?? null,
-      Head: HeadMod?.Head ?? HeadMod?.default ?? null
+  // If the server reported a 404 or 500 error, load the error modules!
+  if (initialContext.is404 || initialContext.is500 || initialContext.errorMessage) {
+    if (clientOptions.errorRoute) {
+      const [ErrorPageMod, ErrorLayoutMod, ErrorHeadMod] = await Promise.all([
+        clientOptions.errorRoute.Page(),
+        clientOptions.errorRoute.Layout?.() ?? null,
+        clientOptions.errorRoute.Head?.() ?? null
+      ])
+      initialView = {
+        Page: ErrorPageMod.Page ?? ErrorPageMod.default,
+        Layout: ErrorLayoutMod?.Layout ?? ErrorLayoutMod?.default ?? null,
+        Head: ErrorHeadMod?.Head ?? ErrorHeadMod?.default ?? null
+      }
+    }
+  } else {
+    // Otherwise, perform the normal route match
+    const matched = matchRoute(pathname, clientOptions.routes)
+    if (matched) {
+      const [PageMod, LayoutMod, HeadMod] = await Promise.all([
+        matched.route.Page(),
+        matched.route.Layout?.() ?? null,
+        matched.route.Head?.() ?? null
+      ])
+      initialView = {
+        Page: PageMod.Page ?? PageMod.default,
+        Layout: LayoutMod?.Layout ?? LayoutMod?.default ?? null,
+        Head: HeadMod?.Head ?? HeadMod?.default ?? null
+      }
+    } else if (clientOptions.errorRoute) {
+      // Fallback: The URL does not exist in the client routing
+      const [ErrorPageMod, ErrorLayoutMod, ErrorHeadMod] = await Promise.all([
+        clientOptions.errorRoute.Page(),
+        clientOptions.errorRoute.Layout?.() ?? null,
+        clientOptions.errorRoute.Head?.() ?? null
+      ])
+      initialView = {
+        Page: ErrorPageMod.Page ?? ErrorPageMod.default,
+        Layout: ErrorLayoutMod?.Layout ?? ErrorLayoutMod?.default ?? null,
+        Head: ErrorHeadMod?.Head ?? ErrorHeadMod?.default ?? null
+      }
+      initialContext.is404 = true
     }
   }
 
