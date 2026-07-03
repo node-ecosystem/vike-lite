5ecf485fa2069d5bcc8b6d3b8a8f04dc73604847
rtritto <ariele.1994@gmail.com>
2026-07-03 14:11:51 +0200

    chore: add client/router and server/abort to build

--- packages/vike-lite/tsdown.config.ts
+++ packages/vike-lite/tsdown.config.ts
@@ -4,7 +4,9 @@ export default {
   entry: {
     'index': 'src/index.ts',
     'vite': 'src/vite-plugin.ts',
+    'client/router': 'src/client/router.ts',
     'server': 'src/server/index.ts',
+    'server/abort': 'src/server/abort.ts',
     '__internal/shared': 'src/__internal/shared/index.ts',
     '__internal/server': 'src/__internal/server.ts'
   }
