e34aa9ee911faf3577a18de81e86f49febd44760
rtritto <ariele.1994@gmail.com>
2026-07-05 20:57:43 +0200

    lint

--- packages/vike-lite-solid/src/global.d.ts
+++ packages/vike-lite-solid/src/global.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-var */
 
 declare module 'virtual:vike-lite-solid/config' {
-  // eslint-disable-next-line unicorn/consistent-boolean-name
   export const hydration: boolean
 }
 
