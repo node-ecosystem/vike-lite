6602208bb8ed9c3b9f9389c516d2267071788815
rtritto <ariele.1994@gmail.com>
2026-07-06 14:33:27 +0200

    TS

--- packages/vike-lite/src/index.ts
+++ packages/vike-lite/src/index.ts
@@ -1,15 +1,18 @@
-export type PageContext<Data = unknown> = {
+type PageContextBase = {
   routeParams: Record<string, string>
   urlOriginal: string
   urlPathname: string
   search: string
-  data?: Data
   title?: string
   nonce?: string
   is404?: boolean
   is500?: boolean
   errorMessage?: string
 }
+
+export type PageContext<Data = unknown> = PageContextBase & (
+  unknown extends Data ? { data?: Data } : { data: Data }
+)
 
 export type DataAsync<Data = unknown> = (pageContext: PageContext) => Promise<Data>
 
