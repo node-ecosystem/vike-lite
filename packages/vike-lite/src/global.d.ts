14dd5694d0aa2f609ef6365cdc46303a676e2209
rtritto <ariele.1994@gmail.com>
2026-07-10 18:01:39 +0200

    TS

--- packages/vike-lite/src/global.d.ts
+++ packages/vike-lite/src/global.d.ts
@@ -5,24 +5,6 @@ declare module 'virtual:routes' {
     onRenderHtml: () => Promise<{ default: (ctx: RenderContext) => Promise<string> }>
     onRenderClient: () => Promise<{ default: (opts: { routes: any[]; errorRoute: any }) => void }>
   }
-  type Route = {
-    path: string
-    page: string
-    layout?: string
-    head?: string
-    // Public flag: visible to both client and server.
-    // The client uses "data" and "title" paths to decide whether to fetch the .pageContext.json
-    // Catch-22:
-    // - exclude import "data" from the client bundle
-    //   but import it in the server bundle to include it in the server bundle
-    //   (it will be available for SSR "data" fetching)
-    // - "data" path indicates if "data" can be fetched by the client
-    // - "title" path indicates if "title" can be fetched by the client
-    // - Flags are present only if errorRoute isn't undefined
-    data?: string
-    title?: string
-    prerender?: string
-  }
   type PageContext = import('./index').PageContext
 
   export const config: Config
