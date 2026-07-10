938ae1c7d39db63529d757f258c1182c17a7e212
rtritto <ariele.1994@gmail.com>
2026-07-10 17:28:35 +0200

    TS

--- packages/vike-lite/src/__internal/shared/index.ts
+++ packages/vike-lite/src/__internal/shared/index.ts
@@ -13,9 +13,4 @@ export interface RenderContext {
   nonce?: string
 }
 
-export type Config = {
-  onRenderHtml: () => Promise<{ default: (ctx: RenderContext) => Promise<string> }>
-  onRenderClient: () => Promise<{ default: (opts: { routes: any[]; errorRoute: any }) => void }>
-}
-
 export { default as matchRoute } from './matchRoute'
