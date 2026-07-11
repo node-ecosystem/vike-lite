d4ae41b9231eaa3b19bd8296cc97974c8224b2d1
rtritto <ariele.1994@gmail.com>
2026-07-11 18:07:29 +0200

    comments

--- packages/vike-lite-solid/src/hooks/useData.tsx
+++ packages/vike-lite-solid/src/hooks/useData.tsx
@@ -5,8 +5,8 @@ import type { PageContext } from 'vike-lite'
 import globalContext from './globalContext'
 
 /**
- * @link https://github.com/vikejs/vike-solid/blob/main/packages/vike-solid/hooks/useData.tsx
  * Hook dedicated to `data` for convenience (returns getter and setter as an array).
+ * @link https://github.com/vikejs/vike-solid/blob/main/packages/vike-solid/hooks/useData.tsx
  */
 export default function useData<Data extends PageContext['data']>(): [Data, SetStoreFunction<Data>] {
   const context = useContext(globalContext.solidContext)!
--- packages/vike-lite-solid/src/hooks/usePageContext.tsx
+++ packages/vike-lite-solid/src/hooks/usePageContext.tsx
@@ -2,11 +2,11 @@ import { useContext } from 'solid-js'
 import type { PageContext } from 'vike-lite'
 import globalContext, { type InternalContextValue } from './globalContext'
 /**
- * @link https://github.com/vikejs/vike-solid/blob/main/packages/vike-solid/hooks/usePageContext.tsx
  * Hook to access the full page context.
  * In SolidJS there is no need to omit `data` for performance reasons:
  * fine-grained reactivity ensures that components only update
  * if they access properties that have actually changed.
+ * @link https://github.com/vikejs/vike-solid/blob/main/packages/vike-solid/hooks/usePageContext.tsx
  */
 export default function usePageContext<Data = unknown>(): PageContext<Data> {
   return (useContext(globalContext.solidContext) as InternalContextValue<Data>).state
