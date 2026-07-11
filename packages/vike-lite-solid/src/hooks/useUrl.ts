1e458959c16eba64c2a119c72c17cd1f8002f995
rtritto <ariele.1994@gmail.com>
2026-07-11 18:07:38 +0200

    lint

--- packages/vike-lite-solid/src/hooks/useUrl.ts
+++ packages/vike-lite-solid/src/hooks/useUrl.ts
@@ -4,8 +4,6 @@ import usePageContext from './usePageContext'
 
 export default function useUrl() {
   const ctx = usePageContext()
-
   const url = createMemo(() => new URL(ctx.urlOriginal, 'http://localhost'))
-
   return url
 }
