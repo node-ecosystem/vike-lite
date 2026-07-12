82e040da3023b4ac98baad20f9a96ca595468ed0
rtritto <ariele.1994@gmail.com>
2026-07-12 16:24:58 +0200

    TS

--- packages/vike-lite-solid/src/hooks/useUrl.ts
+++ packages/vike-lite-solid/src/hooks/useUrl.ts
@@ -1,8 +1,8 @@
-import { createMemo } from 'solid-js'
+import { createMemo, type Accessor } from 'solid-js'
 
 import { usePageContext } from './usePageContext'
 
-export function useUrl() {
+export function useUrl(): Accessor<URL> {
   const ctx = usePageContext()
   const url = createMemo(() => new URL(ctx.urlOriginal, 'http://localhost'))
   return url
