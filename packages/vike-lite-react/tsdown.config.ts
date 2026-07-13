7d26572f056336318d8f70503430b940bdd89581
rtritto <ariele.1994@gmail.com>
2026-07-13 13:25:42 +0200

    fix(vike-lite-react): build

--- packages/vike-lite-react/tsdown.config.ts
+++ packages/vike-lite-react/tsdown.config.ts
@@ -3,6 +3,11 @@ import type { UserConfig } from 'tsdown'
 export default {
   entry: {
     'index': 'src/index.ts',
-    'vite': 'src/vite-plugin.ts'
+    'vite': 'src/vite-plugin.ts',
+    '__internal/client/onRenderClient': 'src/__internal/client/onRenderClient.tsx',
+    '__internal/server/onRenderHtml': 'src/__internal/server/onRenderHtml.tsx'
+  },
+  deps: {
+    neverBundle: [/^virtual:/]
   }
 } satisfies UserConfig
