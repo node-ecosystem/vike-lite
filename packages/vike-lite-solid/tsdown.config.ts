fd1206f9d574bb5ea4e8f4eca05f8d77064477e9
rtritto <ariele.1994@gmail.com>
2026-07-14 09:49:59 +0200

    fix(vike-lite-solid): build

--- packages/vike-lite-solid/tsdown.config.ts
+++ packages/vike-lite-solid/tsdown.config.ts
@@ -1,8 +1,15 @@
 import type { UserConfig } from 'tsdown'
+import solid from 'unplugin-solid/rolldown'
 
 export default {
   entry: {
     'index': 'src/index.ts',
-    'vite': 'src/vite-plugin.ts'
-  }
+    'vite': 'src/vite-plugin.ts',
+    '__internal/client/onRenderClient': 'src/__internal/client/onRenderClient.tsx',
+    '__internal/server/onRenderHtml': 'src/__internal/server/onRenderHtml.tsx'
+  },
+  deps: {
+    neverBundle: [/^virtual:/]
+  },
+  plugins: [solid()]
 } satisfies UserConfig
