1ec38ffe77ede92861a60ecfbbb77a5bbd0095cb
rtritto <ariele.1994@gmail.com>
2026-07-19 12:28:42 +0200

    refactor: update useUrl

--- packages/vike-lite-react/src/hooks/useUrl.tsx
+++ packages/vike-lite-react/src/hooks/useUrl.tsx
@@ -3,5 +3,8 @@ import { usePageContext } from './usePageContext'
 
 export function useUrl(): URL {
   const pageContext = usePageContext()
-  return useMemo(() => new URL(pageContext.urlOriginal), [pageContext.urlOriginal])
+  // The base URL argument is a defensive fallback:
+  // urlOriginal is always absolute in practice,
+  // but parsing shouldn't rely silently on that internal guarantee.
+  return useMemo(() => new URL(pageContext.urlOriginal, 'http://localhost'), [pageContext.urlOriginal])
 }
