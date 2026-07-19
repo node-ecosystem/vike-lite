ab21cf5f8eee5b0524e08e4e01dd5113ba6e28bf
rtritto <ariele.1994@gmail.com>
2026-07-19 23:48:04 +0200

    minimal eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'eslint/config'
 import pluginTypescript from 'typescript-eslint'
-import pluginUnicorn from 'eslint-plugin-unicorn'
-import react from 'eslint-plugin-react'
-import pluginSolid from 'eslint-plugin-solid/configs/typescript'
 import svelte from 'eslint-plugin-svelte'
-import vue from 'eslint-plugin-vue'
 
 export default defineConfig(
   {
     extends: [
-      pluginTypescript.configs.recommended,
-      pluginUnicorn.configs.recommended
+      pluginTypescript.configs.recommended
     ],
     rules: {
       'comma-dangle': [1, 'never'],
@@ -18,37 +13,10 @@ export default defineConfig(
 
       '@typescript-eslint/no-explicit-any': 0,
       '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
-
-      'unicorn/empty-brace-spaces': 0,
-      'unicorn/consistent-boolean-name': 0,
-      'unicorn/dom-node-dataset': 0,
-      'unicorn/filename-case': 0,
-      'unicorn/name-replacements': 0,
-      'unicorn/no-empty-file': 0,
-      'unicorn/no-global-object-property-assignment': 0,
-      'unicorn/no-keyword-prefix': 0,
-      'unicorn/no-nested-ternary': 0,
-      'unicorn/no-null': 0,
-      'unicorn/no-this-outside-of-class': 0,
-      'unicorn/no-top-level-side-effects': 0,
-      'unicorn/numeric-separators-style': 0,
-      'unicorn/prefer-node-protocol': 0
     }
-  },
-  {
-    plugins: { react },
-    files: ['packages/vike-lite-react/**/*.{ts,tsx}']
-  },
-  {
-    files: ['packages/vike-lite-solid/**/*.{ts,tsx}'],
-    extends: [pluginSolid]
   },
   {
     plugins: { svelte },
     files: ['packages/vike-lite-svelte/**/*.{ts,svelte}']
-  },
-  {
-    plugins: { vue },
-    files: ['packages/vike-lite-vue/**/*.ts']
   }
 )
